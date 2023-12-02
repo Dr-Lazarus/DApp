07ae8c167f1f506e69a58a7fc33c6157e959436a
Visshal Natarajan <visshal_natarajan@mymail.sutd.edu.sg>
2023-12-02 17:09:53 +0800

    feat: Requests Transaction

--- src/pages/requests.js
+++ src/pages/requests.js
@@ -1,44 +1,89 @@
-
 // YourPage.js
-import React from 'react';
-import RequestTable from 'views/RequestTable'
-import { useTheme } from '@mui/material/styles';
+import React from "react";
+import { useState, useEffect } from "react";
+import RequestTable from "views/RequestTable";
+import { useTheme } from "@mui/material/styles";
 
+// import React, { useState, useEffect } from 'react';
+// import Box from '@mui/material/Box';
+// import Grid from '@mui/material/Grid';
+// import { useTheme } from '@mui/material/styles';
+// import Main from 'layouts/Main';
+// import Container from 'components/Container';
+// import Hero from 'components/Hero';
+// // import Contact from 'components/Newsletter';
+import FundraiserCard from "blocks/FundraiserCard";
+import FundraiserFactory from "contracts/FundraiserFactory.json";
+import FundraiserContract from "contracts/Fundraiser.json";
+import Web3 from "web3";
 
 const ViewRequests = () => {
-    const theme = useTheme();
+  const theme = useTheme();
+  const [funds, setFunds] = useState([null]);
+  const [contract, setContract] = useState(null);
+  const [accounts, setAccounts] = useState(null);
+  const web3 = new Web3(
+    new Web3.providers.HttpProvider("https://rpc-mumbai.maticvigil.com")
+  );
 
-    // Sample data, replace it with your actual data
-    const exampleData = [
-        {
-          projectName: 'Project A',
-          requestedAmount: 1000.0,
-          availableAmount: 750.0,
-          beneficiaryHash: '0x123456789abcdef',
-        },
-        {
-          projectName: 'Project B',
-          requestedAmount: 2000.0,
-          availableAmount: 1500.0,
-          beneficiaryHash: '0x987654321abcdef',
-        },
-        {
-            projectName: 'Project A',
-            requestedAmount: 500.0,
-            availableAmount: 750.0,
-            beneficiaryHash: '0x123456789abcdef',
-          },
+  useEffect(() => {
+    init();
+    getRequestsCreated();
+  }, []);
 
-        // Add more entries as needed
-      ];
-            
+  const init = async () => {
+    try {
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = FundraiserFactory.networks[networkId];
+      const accounts = await web3.eth.getAccounts();
+      const instance = new web3.eth.Contract(
+        FundraiserFactory.abi,
+        deployedNetwork && deployedNetwork.address
+      );
+      setContract(instance);
+      setAccounts(accounts);
 
-    return (
-        <div>
-            {/* <h1>Your Page</h1> */}
-            <RequestTable data={exampleData} theme={theme} />
-        </div>
-    );
+      const funds = await instance.methods.fundraisers(1000, 0).call();
+
+      setFunds(funds);
+      console.log("------", funds);
+      setFunds(funds);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const getRequestsCreated = async () => {
+    try {
+      console.log("------ HERE --0---- ", funds);
+      const currentBlock = await web3.eth.getBlockNumber();
+      console.log("------ BLOCK ------ ", currentBlock);
+      const fromBlock = Math.max(currentBlock - 1000, 0); // Ensure fromBlock is not negative
+      for (const fundAddress of funds) {
+        const fundContract = new web3.eth.Contract(
+          FundraiserContract.abi,
+          fundAddress
+        );
+        console.log("------ LISTEN ------ ");
+        const events = await fundContract.getPastEvents("RequestCreated", {
+          fromBlock: fromBlock,
+          toBlock: "latest",
+        });
+        console.log("EVENT RE", events);
+
+        // Log each event for this fund
+        events.forEach((event) => console.log(event.returnValues));
+      }
+    } catch (error) {
+      console.error("Error fetching RequestCreated events:", error);
+    }
+  };
+
+  return (
+    <div>
+      {/* <h1>Your Page</h1> */}
+      {/* <RequestTable data={exampleData} theme={theme} /> */}
+    </div>
+  );
 };
 
 export default ViewRequests;
